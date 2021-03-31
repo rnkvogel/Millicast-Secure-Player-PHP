54b7003d0d34bdce22d550e6a5530b46f93b844a
rnkvogel <rvogel@influxis.com>
2021-03-31 15:03:25 -0700

    Add files via upload

--- player/index.php
+++ player/index.php
@@ -1,88 +1,92 @@
 <?php
-$streamName = ($_GET["id"]);
-$label = ($_GET["l"]);
+#$streamName = ($_GET["id"]); //if dynamic from URL or login page
+#$accountId = ($_GET["account"]);
+$url = 'https://api.millicast.com/api/subscribe_token/';
+$headers = array();
+$headers[] = 'Authorization: Bearer YOUR_API_SECRET_KEY';
+$headers[] = 'Content-Type: application/json';
+$streamName = 'YOUR_STREAM_NAME';
+$accountId = 'MILLICAST_ACCOUNT_ID';
+$label = 'YOUR_STREA_LABEL';
+$allowedDomains = 'YOUR_SECURE_DOMAIN';
 error_reporting(E_ALL);
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://api.millicast.com/api/subscribe_token/');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, '{"subscribesAuth": true, "label": "'. $label.'", "streams": [{"streamName":"'. $streamName.'"}]}');
-$headers = array();
-$headers[] = 'Authorization: Bearer FROM_MILLICAT_PORTAL_ACCOUNT_API_TOKEN';
-$headers[] = 'Content-Type: application/json';
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-;
-$result = curl_exec($ch);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+#curl_setopt($ch, CURLOPT_POSTFIELDS, '{"subscribesAuth": true, "label": "'. $label.'", "streams": [{"streamName":"'. $streamName.'"}], "allowedOrigins": ["huhtm4.cloud.influxis.com", "*.millicast.com"]}');
+curl_setopt($ch, CURLOPT_POSTFIELDS, '{"subscribesAuth": true, "label": "'. $label.'", "streams": [{"streamName":"'. $streamName.'"}], "allowedOrigins": ["'.$allowedDomains.'"]}');
+$result = curl_exec($ch);;
+
+$result = json_decode( $result, true);
+
 if (curl_errno($ch)) {
     echo 'Error:' . curl_error($ch);
 }
 curl_close($ch);
-//print_r($result).'<br />';
+#print_r($result).'<br />';//outputs array
+#Just need the token
+$token=  $result['data']['token'];
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-html, body {
-  margin: 0;
-  height: 100%;             /* need for iframe height 100% to work */
-}
-</style>
+<title></title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="css/viewer.css" rel="stylesheet">
+<link href="css/offline.css" rel="stylesheet">
+<!-- <?php echo "?id=". $streamName. "&account=". $accountId. "&token=". $token; ?>  -->
 </head>
-<body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<!-- Standard shim for WebRTC browser compatibility -->
+<script src="js/adapter-latest.js"></script>
+<script src="js/offline.js"></script>
+<script src="js/usercount.js"></script>
 <script>
 
-var result = <?php echo json_encode($result); ?>;
-var obj = JSON.parse(result);
-     // Define recursive function to print nested values
-    function printValues(obj) {
-        for(var k in obj) {
-            if(obj[k] instanceof Object) {
-                printValues(obj[k]);
-            } else {
-                document.write(obj[k] + "<br>");
-            };
-        }
-};
-     // Printing all the values from the resulting object
-    // printValues(obj);
-    // document.write("<hr>");
-    // Printing a single value so time stamp can be added later for expiring token
-    //document.write(obj["data"]["token"] + "<br>");  
-    //document.write(obj["data"]["expiresOn"] + "<br>");
-function getQueryVariable(variable)
-{
-var query = window.location.search.substring(1);
-var vars = query.split("&");
-for (var i=0;i<vars.length;i++) {
-var pair = vars[i].split("=");
-if(pair[0] == variable){return pair[1];}
-       }
-return(false);
-}
+    $(function(){
+         var
+            $online = $('.online'),
+            $offline = $('.offline');
 
- var viewer = "https://viewer.millicast.com/v2?streamId=";
- var accountID = "YOURID" + "/";
- var vid =  getQueryVariable("id");
- var vid_esc = decodeURI(vid);
- var muted = "&muted=false";//works for sound and autoplay
- var token = (obj["data"]["token"]); 
- var http = viewer + accountID + vid + "&token=" + token ;
-	//var http = viewer + accountID + vid + "&token=" + token = muted ; //Autoplay with audio works on Chrome Edge if you had user log into site or interacted with site.
- function myFunction() {
-  document.getElementById("source").src = http;
-}
-//alert(http);
-var a = document.createElement('iframe');
-a.setAttribute('id','video'); // assign an id
-a.src =  http ; //add your iframe path here
-a.width = "100%";
-a.height = "100%";
-a.setAttribute('allowFullscreen','true');
-a.setAttribute('allow', 'autoplay');
-a.setAttribute('frameBorder', '0');
-document.querySelector('body').ap
-</script>
+        Offline.on('confirmed-down', function () {
+            $online.fadeOut(function () {
+                $offline.fadeIn();
+            });
+        });
+
+        Offline.on('confirmed-up', function () {
+        	//internet has been down for a long time.
+
+        	connect();
+             $offline.fadeOut(function () {
+                $online.fadeIn();
+
+            });
+        });
+     });
+
+</script><!-- autoplay = muted autoplay-->
+<div class="wrapper">
+  <input type="hidden" id="token" value=<?php echo $token;?>  >
+    <input type="hidden" id="id"  value=<?php echo $streamName;?> >
+	  <input type="hidden"  id="account" value=<?php echo $accountId;?> >
+
+<video id="player" disablePictureInPicture controlsList="nodownload" autoplay muted playsinline poster="images/starting.png" >
+  <!--   <source src="video/test.mp4" type="video/mp4">   --> </video>
+</div>
+
+<div id="userCntView"><p id="count">viewing: 00</p></div>
+
+<div id="msgView">
+  <h3 id="msgOverlay">One Moment...</h3>
+</div>
+<!-- <h3 style="position: absolute; left: 10px; bottom: 10px;">THIS IS A TEST!!</h3> -->
+<button id="audioBtn" class="btn-audio"  onClick="toggleMute()" type="button"></button>
+</div>
+
+<script src="js/viewer.js"></script>
 </body>
 </html>
