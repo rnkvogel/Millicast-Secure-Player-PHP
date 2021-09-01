65f54afbfb8304ec39e1eceed72881b0fa92c4dc
rnkvogel <rvogel@influxis.com>
2021-09-01 09:39:10 -0700

    Update viewer.js

--- player/js/viewer.js
+++ player/js/viewer.js
@@ -367,7 +367,7 @@
       });
     }
     //connect();
-    // get a list of Xirsys ice servers.
+    // get a list of  ice servers.  //Twil
     getICEServers()
       .then(list => {
         iceServers = list;
